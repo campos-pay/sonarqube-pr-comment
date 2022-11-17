a213284d39768eec68b85ef99fb8a8d1e86c1526
Guerlielton <guerlielton@gmail.com>
2022-11-16 23:08:32 -0300

    Update project-status.sh

--- script/project-status.sh
+++ script/project-status.sh
@@ -8,14 +8,14 @@ project_status="$(curl -s -u ${SONAR_TOKEN}: -G --data-urlencode --data-urlencod
 ${projectStatusUrl})"
 
 codeOk=$(jq -r '.projectStatus.conditions[] | select(.status=="OK") | "\n✅Status: " + .status, "MetricKey: " + .metricKey, "Comparator: " + .comparator, "ErrorThreshold: " + .errorThreshold, "ActualValue: " + .actualValue' <<< "$project_status")
-codeOk="${codeOk//'%'/'%25'}"
-codeOk="${codeOk//$'\n'/'%0A'}"
-codeOk="${codeOk//$'\r'/'%0D'}"
+# codeOk="${codeOk//'%'/'%25'}"
+# codeOk="${codeOk//$'\n'/'%0A'}"
+# codeOk="${codeOk//$'\r'/'%0D'}"
 
 codeFail=$(jq -r '.projectStatus.conditions[] | select(.status=="ERROR") | "\n💣Status: " + .status, "MetricKey: " + .metricKey, "Comparator: " + .comparator, "ErrorThreshold: " + .errorThreshold, "ActualValue: " + .actualValue' <<< "$project_status")
-codeFail="${codeFail//'%'/'%25'}"
-codeFail="${codeFail//$'\n'/'%0A'}"
-codeFail="${codeFail//$'\r'/'%0D'}"
+# codeFail="${codeFail//'%'/'%25'}"
+# codeFail="${codeFail//$'\n'/'%0A'}"
+# codeFail="${codeFail//$'\r'/'%0D'}"
 
 error="ERROR CONFIGURATION"
 
