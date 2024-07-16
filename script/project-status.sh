bb502b7bcf6347ed79bb1bd40f3833c5c68bf260
guerlielton <guerlielton@gmail.com>
2024-07-16 23:27:45 +0100

    Update project-status.sh

--- script/project-status.sh
+++ script/project-status.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/env bash
 
 qualityGateUrl="${SONAR_HOST_URL}/api/qualitygates/project_status?projectKey=${SONAR_PROJECTKEY}"
-qualityGateStatus="$(curl --silent --fail --show-error --user "${SONAR_TOKEN}": "${qualityGateUrl}" | jq -r '.projectStatus.status')"
+project_status=$(curl --silent --fail --show-error --user "${SONAR_TOKEN}": "${qualityGateUrl}")
 
-projectStatusUrl="${SONAR_HOST_URL}/api/qualitygates/project_status?projectKey=${SONAR_PROJECTKEY}"
-project_status="$(curl -s -u ${SONAR_TOKEN}: -G --data-urlencode --data-urlencode \
-${projectStatusUrl})"
+qualityGateStatus=$(jq -r '.projectStatus.status' <<< "$project_status")
 
 codeOk=$(jq -r '.projectStatus.conditions[] | select(.status=="OK") | "\n✅Status: " + .status, "MetricKey: " + .metricKey, "Comparator: " + .comparator, "ErrorThreshold: " + .errorThreshold, "ActualValue: " + .actualValue' <<< "$project_status")
 
@@ -14,14 +12,13 @@ codeFail=$(jq -r '.projectStatus.conditions[] | select(.status=="ERROR") | "\n�
 error="ERROR CONFIGURATION"
 
 codeValidation () {
-
-if [[ ${qualityGateStatus} == "OK" ]]; then
-     echo "👋 Hey Quality Gate has PASSED.$codeOk"
-elif [[ ${qualityGateStatus} == "ERROR" ]]; then
-     echo "👋 Hey Quality Gate has FAILED.$codeFail"
-else
-   echo "quality_check=${error}" >> $GITHUB_OUTPUT
-fi
+  if [[ ${qualityGateStatus} == "OK" ]]; then
+    echo "👋 Hey, the Quality Gate has PASSED.$codeOk"
+  elif [[ ${qualityGateStatus} == "ERROR" ]]; then
+    echo "👋 Hey, the Quality Gate has FAILED.$codeFail"
+  else
+    echo "quality_check=${error}" >> $GITHUB_OUTPUT
+  fi
 }
 
 result=$(codeValidation)
@@ -29,4 +26,5 @@ result=$(codeValidation)
 echo "quality_check<<EOF" >> $GITHUB_OUTPUT
 echo "$result" >> $GITHUB_OUTPUT
 echo "EOF" >> $GITHUB_OUTPUT
+
 
