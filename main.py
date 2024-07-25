5a4321c6411f85e03b07971bb04328470ac867d6
guerlielton <guerlielton@gmail.com>
2024-07-25 23:21:50 +0100

    feature comment in table format

--- main.py
+++ main.py
@@ -11,55 +11,44 @@ GITHUB_TOKEN = os.getenv('GITHUB_TOKEN')  # GitHub token
 REPO_NAME = os.getenv('GITHUB_REPOSITORY')  # Ex: "user/repository"
 PR_NUMBER = os.getenv('PR_NUMBER')  # Pull Request number
 
-def get_quality_gate_status():
-    quality_gate_url = f"{SONAR_HOST_URL}/api/qualitygates/project_status?projectKey={SONAR_PROJECTKEY}"
-    # Make the request to the SonarQube API
-    response = requests.get(quality_gate_url, auth=(SONAR_TOKEN, ''))
+def get_sonar_measures():
+    measures_url = f"{SONAR_HOST_URL}/api/measures/component?component={SONAR_PROJECTKEY}&metricKeys=security_hotspots_reviewed,duplicated_lines_density,lines,coverage,security_hotspots,lines_to_cover,bugs,code_smells,vulnerabilities"
+    response = requests.get(measures_url, auth=(SONAR_TOKEN, ''))
     response.raise_for_status()
     
-    project_status = response.json()
-    quality_gate_status = project_status['projectStatus']['status']
-    
-    print(f"Quality gate status retrieved: {quality_gate_status}")
-    return quality_gate_status, project_status
+    measures = response.json()
+    print(f"Métricas do SonarQube recuperadas: {measures}")
+    return measures['component']['measures']
 
-def extract_code_details(project_status):
-    # Extract all conditions regardless of their status
-    conditions = project_status['projectStatus']['conditions']
-
-    # Start creating a Markdown table
-    table = "\n| **Metric** | **Value** |\n|------------|-----------|\n"
+def format_measures_to_table(measures):
+    # Inicia a criação de uma tabela em Markdown
+    table = "\n| **Métrica** | **Valor** |\n|------------|-----------|\n"
     
-    # Fill the table with metric details
-    for condition in conditions:
-        print(f"Processing condition: {condition}")  # Debugging output
-        metric = condition['metricKey'].replace('_', ' ')
-        actual_value = condition['actualValue']
-        if isinstance(actual_value, (int, float)):
-            actual_value = f"{actual_value:.1f}" if isinstance(actual_value, float) else str(actual_value)  # Format floats to 1 decimal place
-        table += f"| {metric} | {actual_value} |\n"
+    # Preenche a tabela com os detalhes das métricas
+    for measure in measures:
+        metric = measure['metric'].replace('_', ' ')
+        value = measure['value']
+        table += f"| {metric} | {value} |\n"
     
     return table
 
 def code_validation():
-    quality_gate_status, project_status = get_quality_gate_status()
-
-    # Construct the result with the overall status and the detailed metrics table
-    status_emoji = '✅' if quality_gate_status == "OK" else '💣'
-    result = f"{status_emoji} **Status: {quality_gate_status}**\n"
-    details_table = extract_code_details(project_status)
-    result += details_table
-
+    measures = get_sonar_measures()
+    details_table = format_measures_to_table(measures)
+    
+    # Cria o resultado final com a tabela das métricas
+    result = f"✅ **Status: OK**\n{details_table}"
+    
     return result
 
 def comment_on_pull_request(body):
-    # Authenticate with GitHub
+    # Autentica com o GitHub
     g = Github(GITHUB_TOKEN)
     repo = g.get_repo(REPO_NAME)
     pull_request = repo.get_pull(int(PR_NUMBER))
 
-    print(f"Commenting on Pull Request #{PR_NUMBER}.")
-    # Comment on the Pull Request
+    print(f"Comentando na Pull Request #{PR_NUMBER}.")
+    # Comenta na Pull Request
     pull_request.create_issue_comment(body)
 
 if __name__ == "__main__":
